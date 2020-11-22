3a8b7e7bec0cf0501e7eea1210fa47628be9bc38
Paloma Passos <palomabcp06@gmail.com>
2020-11-22 00:17:48 -0300

    feat: add funções de requisição

---
+++ src/Components/Utils/requisicoes.js
@@ -0,0 +1,14 @@
+export function fazerRequisicaoGET(url) {
+	fetch(url).then((resposta) => resposta.json());
+}
+
+export function fazerOutrasRequisicoes(url, metodo, conteudo, token) {
+	return fetch(url, {
+		method: metodo,
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: token && `Bearer ${token}`,
+		},
+		body: JSON.stringify(conteudo),
+	}).then((resposta) => resposta.json());
+}
