7b9e673a1b8fa6d6ad9903c3d0609581c817ad10
Paloma Passos <palomabcp06@gmail.com>
2020-11-22 00:11:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Login } from "../src/Components/Login";
+import { Cadastro } from "../src/Components/Cadastro";
+
+function App() {
+	return (
+		<div className="App">
+			<Login></Login>
+		</div>
+	);
+}
+
+export default App;
