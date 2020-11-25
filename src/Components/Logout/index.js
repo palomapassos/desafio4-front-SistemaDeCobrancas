0fbfbb1654f761c56b94fa422c76c0c62db2691d
Paloma Passos <palomabcp06@gmail.com>
2020-11-24 23:01:21 -0300

    feat: add componente de logout

---
+++ src/Components/Logout/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import cifrao from "../Assets/cifrao.svg";
+import userIcon from "../Assets/user.png";
+import deslogarIcon from "../Assets/logout.svg";
+import { LoginContainer } from "../../App";
+import "./styles.css";
+
+export function Logout(props) {
+	const [deslogar, setDeslogar] = React.useState(false);
+	const { logout } = LoginContainer.useContainer();
+
+	return (
+		<div className="container">
+			<div className="saldo">
+				<div>
+					<img src={cifrao} alt="cifrão" /> Saldo em conta
+				</div>
+				<div>
+					R$ <span>0,00</span>
+				</div>
+			</div>
+			<button
+				className="deslogar"
+				type="button"
+				onClick={() => {
+					setDeslogar(!deslogar);
+				}}
+			>
+				<img src={userIcon} alt="user" />
+				{deslogar ? (
+					<button
+						className="deslogarPagina"
+						type="button"
+						onClick={() => {
+							logout();
+						}}
+					>
+						<img src={deslogarIcon} alt="Deseja deslogar?" />
+						<span>Deslogar</span>
+					</button>
+				) : undefined}
+			</button>
+		</div>
+	);
+}
