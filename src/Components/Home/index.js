ce89d1793e115e2cfa2ebcbab6e8c59208873808
Paloma Passos <paloma.passos@cubos.io>
2020-11-28 07:09:51 -0300

    feat: add componente de container de dados

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import { Menu } from "../Menu";
 import { Logout } from "../Logout";
 import { DadosHome } from "../DadosHome";
-
+import { ContainerEditarCriar } from "../ContainerCriarEditar";
 export function Home() {
 	return (
 		<div className="home">
