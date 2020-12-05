a246a4e03f6d783b0d0152ebbfcf2a584227daab
Paloma Passos <paloma.passos@cubos.io>
2020-12-04 23:32:50 -0300

    feat: add mensagem de erro

--- src/Containers/Cadastro/index.js
+++ src/Containers/Cadastro/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import "./mediaqueries.css";
 import { fazerRequisicoes } from "../../Utils/requisicoes.js";
@@ -8,6 +8,10 @@ import { Link } from "react-router-dom";
 
 export function Cadastro() {
 	const { register, handleSubmit } = useForm();
+	const [mensagem, setMensagem] = useState({
+		type: "sucesso" || "erro",
+		mensagem: "",
+	});
 	return (
 		<div className="telaCadastro">
 			<div className="cadastro">
@@ -20,7 +24,7 @@ export function Cadastro() {
 						const email = data.email;
 						const senha = data.senha;
 						fazerRequisicoes(
-							"https://cubos-desafio-4.herokuapp.com/usuarios",
+							"http://cubos-desafio-4.herokuapp.com/usuarios",
 							"POST",
 							{
 								email,
@@ -29,11 +33,17 @@ export function Cadastro() {
 							}
 						)
 							.then(({ dados }) => {
-								console.log(dados);
+								if (dados.status === "sucesso") {
+									setMensagem({ type: "sucesso", mensagem: dados.message });
+								} else {
+									console.log({ dados });
+									setMensagem({ type: "erro", mensagem: dados.message });
+								}
 							})
 							.catch((err) => {
-								console.log(err);
-								alert("Algo deu errado");
+								// console.log(err);
+								// alert("Erro", err.messagem);
+								// alert(err.message);
 							});
 					})}
 				>
@@ -49,6 +59,7 @@ export function Cadastro() {
 						Senha
 						<input name="senha" type="password" ref={register} />
 					</label>
+					{(mensagem.type = "erro" && <span>{mensagem.mensagem}</span>)}
 					<div className="botao">
 						<button className="submit">Criar conta</button>
 					</div>
