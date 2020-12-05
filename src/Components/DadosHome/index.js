3034e4e5c51ff518e0f746df35e94fd9a161245b
Paloma Passos <paloma.passos@cubos.io>
2020-12-04 21:43:02 -0300

    feat: add componentes ao dados Home

--- src/Components/DadosHome/index.js
+++ src/Components/DadosHome/index.js
@@ -4,9 +4,25 @@ import "./mediaqueries.css";
 import { ContainerCard } from "../ContainerCard";
 import { RetanguloCard } from "../RetanguloCard";
 import iconPessoas from "../../Assets/iconPessoas.svg";
+import iconDinheiro from "../../Assets/iconDinheiro.svg";
+import { fazerRequisicoes } from "../../Utils/requisicoes";
+import { LoginContainer } from "../../App";
 
 export function DadosHome() {
 	const [clicado, setClicado] = React.useState("mes");
+	const { token } = LoginContainer.useContainer();
+	const [relatorio, setRelatorio] = React.useState([]);
+
+	React.useEffect(() => {
+		fazerRequisicoes(
+			"https://cubos-desafio-4.herokuapp.com/relatorios",
+			"GET",
+			undefined,
+			token
+		).then(({ dados }) => {
+			setRelatorio(dados);
+		});
+	}, []);
 	return (
 		<div className="bodyHome">
 			<div className="filtrosHome">
@@ -31,12 +47,33 @@ export function DadosHome() {
 			</div>
 			<div className="cardsHome">
 				<ContainerCard tituloCard="Clientes" iconeCard={iconPessoas}>
-					<RetanguloCard cor="vermelho" tipo="Inadimplentes" numero="0" />
-					<RetanguloCard cor="vermelho" tipo="Inadimplentes" numero="0" />
+					<RetanguloCard
+						cor="verde"
+						tipo="Adimplentes"
+						numero={relatorio.qtdClientesAdimplentes}
+					/>
+					<RetanguloCard
+						cor="vermelho"
+						tipo="Inadimplentes"
+						numero={relatorio.qtdClientesInadimplentes}
+					/>
 				</ContainerCard>
-				<ContainerCard tituloCard="Clientes" iconeCard={iconPessoas}>
-					<RetanguloCard cor="vermelho" tipo="Inadimplentes" numero="0" />
-					<RetanguloCard cor="vermelho" tipo="Inadimplentes" numero="0" />
+				<ContainerCard tituloCard="Cobranças" iconeCard={iconDinheiro}>
+					<RetanguloCard
+						cor="verde"
+						tipo="Pagas"
+						numero={relatorio.qtdBoletosPagos}
+					/>
+					<RetanguloCard
+						cor="vermelho"
+						tipo="Vencidas"
+						numero={relatorio.qtdBoletosVencidos}
+					/>
+					<RetanguloCard
+						cor="azul"
+						tipo="Pendentes"
+						numero={relatorio.qtdBoletosNaoPagos}
+					/>
 				</ContainerCard>
 			</div>
 		</div>
