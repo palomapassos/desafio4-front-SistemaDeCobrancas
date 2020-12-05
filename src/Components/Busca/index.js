be1be48d0d7aa25caf8a074f9c4a6e7a3e71c1e4
Paloma Passos <paloma.passos@cubos.io>
2020-12-04 21:39:25 -0300

    feat: add lógica da área de busca

--- src/Components/Busca/index.js
+++ src/Components/Busca/index.js
@@ -1,16 +1,36 @@
 import React from "react";
 import "./styles.css";
 import lupa from "../../Assets/busca.svg";
+import { Button } from "../../Components/Button";
+import { useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export function Busca(props) {
-	const { placeholder } = props;
+	const { placeholder, fazerFiltro, setBusca } = props;
+	const { register, handleSubmit } = useForm();
+	const history = useHistory();
+
+	function aoClicar() {
+		history.push("/criarCliente");
+	}
 	return (
 		<div className="busca">
-			<input placeholder={placeholder} />
-			<button>
-				<img src={lupa} alt="Pesquisar" />
-				Buscar
-			</button>
+			<Button tipo="button" classe="vazado addCliente" aoClicar={aoClicar}>
+				Adicionar cliente
+			</Button>
+			<form
+				onSubmit={handleSubmit((data) => {
+					fazerFiltro(data.busca);
+					setBusca(data.busca !== "");
+				})}
+				className="areaBusca"
+			>
+				<input ref={register} name="busca" placeholder={placeholder} />
+				<button type="submit" className="filtroBusca">
+					<img src={lupa} alt="Pesquisar" />
+					Buscar
+				</button>
+			</form>
 		</div>
 	);
 }
